0f3138d9e079dc6647706666e2b0ff44c7070199
Mack Foggia <61202392+mackfoggia@users.noreply.github.com>
2021-06-23 22:16:14 +0000

    Split full input into individual strings

--- domletters.py
+++ domletters.py
@@ -3,12 +3,15 @@
 # Mack Foggia - CS 461P - HW 1 - 6/28/2021
 
 
-x = ""
+fullInput = ""
 
 while(1):
   try:
-    x += input()
+    fullInput += input()
+    fullInput += ' '
   except:
     break
 
-print(x)
+inputList = fullInput.split(' ')
+
+print(inputList)
