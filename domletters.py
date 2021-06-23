6e773527cdb106bac8b31789e4fce854716d6103
Mack Foggia <61202392+mackfoggia@users.noreply.github.com>
2021-06-23 22:48:35 +0000

    Comments, cleaning, and print result

--- domletters.py
+++ domletters.py
@@ -5,30 +5,43 @@
 
 fullInput = ""
 
+# Read in all input into a single string
 while(1):
   try:
     fullInput += input()
     fullInput += ' '
   except:
     break
+
+# Get rid of upper case letters
 fullInput = fullInput.lower()
 
+# Split into a list of strings around spaces
 inputList = fullInput.split(' ')
 
-print(inputList)
+
 
 total = 0
+# Loop through each word
 for word in inputList:
+  # Check if it is a valid word 
   if word.isalpha():
+    # Reset count dict for each new word
     count = {}
     for char in word:
+      # Count instances of each char
       if char in count:
         count[char] += 1
       else:
         count[char] = 1
-  
+
+    # Find the max count and add it to total 
+    # There's probably a slicker way to do this but it works
     max = 0
     for i, j in count.items():
       if j > max:
         max = j
-    print(max)
+    total += max
+
+# Output result
+print(total)
